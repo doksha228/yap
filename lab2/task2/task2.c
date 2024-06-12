bd95ebc5e25cb5954a9cb402c1b0aa78e8ca0a9a
Daria <dasavasileva04@gmail.com>
2024-06-12 17:45:42 +0400

    .y

--- lab2/task2/task2.c
+++ lab2/task2/task2.c
@@ -1,31 +1,46 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-int main(){
-    srand(time(NULL));
-    FILE *output_file;
-    int size;
-    printf("Enter the array size\n");
-    scanf("%d", &size);
-    
-    int array[size];
-    for (int i = 0; i < size; i++){
-        array[i] = rand()%100;
-        printf("%d\t", array [i]);
+#define ARRAY_SIZE 10
+
+void reverseArray(int arr[], int size) {
+    for (int i = 0; i < size / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
     }
-    
-    output_file = fopen("file1", "w");
-    if(output_file == NULL){
-        printf("Error, can't create file\n");
+}
+
+int main() {
+    int random_numbers[ARRAY_SIZE];
+    srand(time(NULL));
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        random_numbers[i] = rand() % 100;  // Генерируем случайное число от 0 до 99
+    }
+
+    printf("Сгенерированный массив случайных чисел:\n");
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        printf("%d ", random_numbers[i]);
+    }
+    printf("\n");
+
+    reverseArray(random_numbers, ARRAY_SIZE);
+
+    FILE *file = fopen("random_numbers.txt", "w");
+    if (file == NULL) {
+        printf("Ошибка открытия файла.\n");
         return 1;
     }
 
-    for (int i = 0; i < size; i++){
-        fprintf(output_file, "%d\t", array[size-i-1]);
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        fprintf(file, "%d\n", random_numbers[i]);
     }
-    
-    fclose(output_file);
-    return 0;
 
+    fclose(file);
+
+    printf("Массив записан в файл 'random_numbers.txt' в обратном порядке.\n");
+
+    return 0;
 }
