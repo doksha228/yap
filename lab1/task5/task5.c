bf5aa0cee4ccfe07e364fb8280711268d7689466
Daria <dasavasileva04@gmail.com>
2024-06-12 17:39:26 +0400

    .y

--- lab1/task5/task5.c
+++ lab1/task5/task5.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Использование: %s <имя_файла>\n", argv[0]);
+        return 1;
+    }
 
-int main(int argc, char *argv[]){
-   if(argc < 2){
-      printf("Error, try again\n");
-      return 1;
-   }
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Ошибка открытия файла");
+        return 1;
+    }
 
-   FILE *file = fopen(argv[1], "r");
-   if (file==NULL){
-      printf("Error, file is empty... Try again!\n");
-      return 1;
-   }
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        putchar(c);
+    }
 
-   char ch;
-   while ((ch = fgetc(file)) != EOF){
-      putchar(ch);
-      
-   }fclose(file);
+    fclose(file);
+    return 0;
 }
